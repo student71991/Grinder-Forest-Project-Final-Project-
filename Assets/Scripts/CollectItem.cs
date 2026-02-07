31b095061e235aafbc59059d42c0a6811a2720ec
Yoocodehash <lukaszkucharczyk263@gmail.com>
2026-02-07 19:29:07 +0100

    Audio

--- Assets/Scripts/CollectItem.cs
+++ Assets/Scripts/CollectItem.cs
@@ -7,10 +7,13 @@ public class CollectItem : MonoBehaviour
 {
     private int collectibles = 0;
 
+    [SerializeField] private AudioSource collectSoundEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Collectibles"))
         {
+            collectSoundEffect.Play();
             Destroy(collision.gameObject);
             collectibles++;
         }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,8 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody2D rb;
 
+    [SerializeField] private AudioSource jumpSoundEffect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
             isJumping = true;
+            jumpSoundEffect.Play();
         }
 
 
